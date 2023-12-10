06ad5f9e25604cda8f3ba1a37f3b2c2cde52e36c
Atul Singh <jackavins.atul@gmail.com>
2023-12-10 16:09:41 +0530

    add config.xml file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./pre-build.sh
-docker run --name cordova-build -v "$(pwd)/www:/tmp/app/www" -v "$(pwd)/app/PlayStore:/tmp/app/PlayStore" -v "$(pwd)/app/build.json:/tmp/app/build.json" jackavins/cordova:mypin bash -c "rm -rf platforms && cordova platform add android && cordova build android"
+docker run --name cordova-build -v "$(pwd)/www:/tmp/app/www" -v "$(pwd)/app/PlayStore:/tmp/app/PlayStore" -v "$(pwd)/app/build.json:/tmp/app/build.json" -v "$(pwd)/app/config.xml:/tmp/app/config.xml" jackavins/cordova:mypin bash -c "rm -rf platforms && cordova platform add android && cordova build android"
 docker cp cordova-build:/tmp/app/platforms/android/app/build/outputs/apk/debug/app-debug.apk tmp
 ./post-build.sh

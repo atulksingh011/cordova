719f21eb476856b08d2e7f9d71c0b80b8936f4f6
Atul Singh <jackavins.atul@gmail.com>
2023-12-24 19:00:35 +0530

    update to v2.0.16

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./pre-build.sh
-docker run --name cordova-build -v "$(pwd)/www:/tmp/app/www" -v "$(pwd)/app/PlayStore:/tmp/app/PlayStore" -v "$(pwd)/app/build.json:/tmp/app/build.json" -v "$(pwd)/app/config.xml:/tmp/app/config.xml" jackavins/cordova:mypin bash -c "rm -rf platforms && cordova platform add android && cordova build android"
+docker run --name cordova-build -v "$(pwd)/app/config.xml:/tmp/app/config.xml" -v "$(pwd)/www:/tmp/app/www" -v "$(pwd)/app/PlayStore:/tmp/app/PlayStore" -v "$(pwd)/app/build.json:/tmp/app/build.json" -v "$(pwd)/app/config.xml:/tmp/app/config.xml" jackavins/cordova:mypin bash -c "rm -rf platforms && cordova platform add android && cordova build android"
 docker cp cordova-build:/tmp/app/platforms/android/app/build/outputs/apk/debug/app-debug.apk tmp
 docker container stop cordova-build
 docker container rm cordova-build
--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./pre-build.sh
-docker run --name cordova-release -v "$(pwd)/www:/tmp/app/www" -v "$(pwd)/app/PlayStore:/tmp/app/PlayStore" -v "$(pwd)/app/build.json:/tmp/app/build.json" jackavins/cordova:mypin bash -c "rm -rf platforms && cordova platform add android && cordova build --release android"
+docker run --name cordova-release -v "$(pwd)/app/config.xml:/tmp/app/config.xml" -v "$(pwd)/www:/tmp/app/www" -v "$(pwd)/app/PlayStore:/tmp/app/PlayStore" -v "$(pwd)/app/build.json:/tmp/app/build.json" jackavins/cordova:mypin bash -c "rm -rf platforms && cordova platform add android && cordova build --release android"
 docker cp cordova-release:/tmp/app/platforms/android/app/build/outputs/bundle/release/app-release.aab tmp
 docker container stop cordova-release
 docker container rm cordova-release
